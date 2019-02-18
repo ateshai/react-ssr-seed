954c8638f3bba4775194e029f447c6562f77b273
Aykut Ateş <ateshai@gmail.com>
2019-02-18 14:15:46 +0300

    actions path fixed on home component

--- src/common/pages/Home/index.js
+++ src/common/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { increment, decrement, set } from "../../actions";
+import { increment, decrement, set } from "../../store/actions";
 import Header from "../../components/Header";
 
 class Home extends Component {
