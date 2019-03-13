8a7c73edd6e4357d9c2a6c5668369e2428059188
Aykut Ateş <ateshai@gmail.com>
2019-03-13 17:57:03 +0300

    storybook viewport plugin added

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,34 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { addParameters } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 addDecorator(withInfo);
+
+const viewportOptions = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px',
+    },
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px',
+    },
+  },
+};
+
+addParameters({ viewport: { 
+    viewports : {
+      ...INITIAL_VIEWPORTS,
+      ...viewportOptions 
+    }
+  }
+});
 
 function loadStories() {
   require('../stories');
